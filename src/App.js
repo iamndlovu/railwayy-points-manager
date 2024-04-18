8350b237b95503a5261dbcbd9fdcabf111374723
Pardon Ndlovu <pndlovu385@icloud.com>
2024-04-18 16:17:03 +0200

    delete unused files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import Welcome from './components/Welcome';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Welcome />
+      {/* <header>
+        <h1 className='App-name'>RAILROAD POINTS STATUS WARNING SYSTEM</h1>
+      </header> */}
     </div>
   );
 }
